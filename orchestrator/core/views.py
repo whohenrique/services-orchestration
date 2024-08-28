967925019abb6cdec7fede3bee0388437a848bdd
Valéria <vvqf2016@gmail.com>
2024-08-28 01:48:18 -0300

    chore: added try and except validation

--- orchestrator/core/views.py
+++ orchestrator/core/views.py
@@ -5,42 +5,45 @@ from rest_framework.response import Response
 class PurchaseProductView(views.APIView):
 
     def post(self, request, *args, **kwargs):
-        product_id = request.data['product_id']
-        quantity = request.data['quantity']
+        product_id = request.data.get('product_id')
+        quantity = request.data.get('quantity')
+
+        #TODO 1. Verificar disponibilidade do estoque no Inventory Service
 
 
-    #TODO 1. Verificar disponibilidade do estoque no Inventory Service
-
-
-
-    #2. Criar o pedido no Product Service
-
-        order_response = requests.post('http://localhost:8001/api/orders/', json={
-            'product_id': product_id,
-            'quantity': quantity
-        })
-        
-        if order_response.status_code != 200:
-            return Response({'error': 'Failed to create order'}, status=400)
-
-        order_id = order_response.json().get('order_id')
-
-
-    # 3. Reservar o pedido no Inventory Service
-
-        inventory_reserve_response = requests.post('http://localhost:8002/api/inventory/reserve/', json={
+        # 2. Criar o pedido no Product Service
+        try:
+            order_response = requests.post('http://localhost:8001/api/orders/', json={
                 'product_id': product_id,
                 'quantity': quantity
             })
+            order_response.raise_for_status()
 
-        if inventory_reserve_response.status_code != 200:
-            return Response({'error': 'Failed to reserve inventory'}, status=400)
+            if order_response.status_code != 200:
+                return Response({'error': 'Failed to create order'}, status=400)
 
+        except requests.RequestException:
+            return Response({'error': 'Failed to create order'}, status=500)
 
+        order_id = order_response.json().get('order_id')
 
-    #TODO 4. Processar o pagamento no Payment Service
+        # 3. Reservar o inventário no Inventory Service
+        try:
+            inventory_reserve_response = requests.post('http://localhost:8002/api/inventory/reserve/', json={
+                'product_id': product_id,
+                'quantity': quantity
+            })
+            inventory_reserve_response.raise_for_status()
+
+            if inventory_reserve_response.status_code != 200:
+                return Response({'error': 'Failed to reserve inventory'}, status=400)
+            
+        except requests.RequestException:
+            return Response({'error': 'Failed to reserve inventory'}, status=500)
+
         
+
+        #TODO 4. Processar o pagamento no Payment Service
+    
 
         return Response({'status': 'Purchase Completed'})
-
-        
